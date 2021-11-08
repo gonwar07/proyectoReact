4ab170d9c6b880fca179ca802ff85170acd9a091
gonwar07 <gonwar1988@gmail.com>
2021-11-08 20:30:26 -0300

    Ejercicio Clase 2 - Menu navegación

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+
+const NavBar = () => {
+
+    return(
+    
+    <header className="header">
+        <nav className="menu">
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Productos</a></li>
+            <li><a href="#">Sucursales</a></li>
+            <li><a href="#">Contacto</a></li>
+        </nav>
+    </header>)
+
+}
+
+export default NavBar
